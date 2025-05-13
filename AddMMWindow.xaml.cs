8a7a89661c1e7c7a49310ba6da7b4be987dac473
Adraxas Nazaron <dragonjunk45@gmail.com>
2025-05-13 10:55:59 -0700

    Added dialog for adding new mod managers

---
+++ AddMMWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NXM_Handler
+{
+    public partial class AddMMWindow : Window
+    {
+        public AddMMWindow()
+        {
+            ((TextBlock)MainGrid.Children[MainGrid.Children.IndexOf(MMName) + 1]).Text = "Mod Manager Name (Leave blank for executable name)";
+            ((TextBlock)MainGrid.Children[MainGrid.Children.IndexOf(MMPath) + 1]).Text = "Path to executable";
+            InitializeComponent();
+        }
+        private void SelectPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = "exe"
+            };
+            MMPath.Text = dialog.FileName;
+        }
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            Storage.AddNewModManager(new ModManager(MMName.Text, MMPath.Text));
+        }
+    }
+}
